35e46dac63e5274037222048675f178d97cbe0ea
timdegroot05 <336795@student.mboutrecht.nl>
2023-03-14 13:12:37 +0100

    index toegevoegd

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Zangeres</title>
+</head>
+<body>
+<p>View-1</p>
+    <h1>Top 5 rijkste zangeressen ter wereld</h1>
+    <div class="main">
+                <div class="naam">
+                    <h2>Naam</h2>
+                    <label for="naam">Rihanna</label><br>
+                    <label for="Madonna">Madonna</label><br>
+                    <label for="taylor">Taylor Swift</label><br>
+                    <label for="Beyonce">Beyonce</label><br>
+                    <label for="Jennifer">Jennifer Lopez</label>
+                </div>
+            
+                <div class="netto">
+                    <h2>Nettowaarde (miljoen)</h2>
+                    <label for="Rihanna">1400</label><br>
+                    <label for="Madonna">575</label><br>
+                    <label for="taylor">570</label><br>
+                    <label for="Beyonce">420</label><br>
+                    <label for="Jennifer">400</label>
+                </div>
+            
+                <div class="land">
+                    <h2>Land</h2>
+                    <label for="Rihanna">Barbados</label><br>
+                    <label for="Madonna">Verenigde Staten</label><br>
+                    <label for="taylor">Verenigde Staten</label><br>
+                    <label for="Beyonce">Verenigde Staten</label><br>
+                    <label for="Jennifer">Verenigde Staten</label>
+                </div>
+           
+                <div class="mobielenummer">
+                    <h2>Mobiel</h2>
+                    <label for="Rihanna">+1246 2400 1862400</label><br>
+                    <label for="Madonna">+1 3425 18576</label><br>
+                    <label for="taylor">+1 5876 236512</label><br>
+                    <label for="Beyonce">+1 6794 908465</label><br>
+                    <label for="Jennifer">+1 3131 857345</label>
+                </div>
+            
+                <div class="leeftijd">
+                    <h2>Leeftijd</h2>
+                    <label for="Rihanna">35</label><br>
+                    <label for="Madonna">64</label><br>
+                    <label for="taylor">33</label><br>
+                    <label for="Beyonce">41</label><br>
+                    <label for="Jennifer">53</label>
+                </div>
+    </div>
+</body>
+</html>
