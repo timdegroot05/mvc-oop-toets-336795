2085a6ac655356b10e1ef5b35ee573ad727f1837
timdegroot05 <336795@student.mboutrecht.nl>
2023-03-14 12:10:17 +0100

    conectie gemaakt met de database

--- database/read.php
+++ database/read.php
@@ -1,2 +1,15 @@
 <?php
 include('config.php');
+
+$dsn = "mysql:host=$dbHost;dbname=$dbName;charset=UTF8";
+
+try {
+    $pdo = new PDO($dsn, $dbUser, $dbPass);
+    if ($pdo) {
+        echo "Er is een verbinging gemaakt met de mysql server";
+    } else {
+        echo "Er is een interne server error opgetreden";
+    }
+} catch(PDOException $e) {
+    echo $e->getMessage();
+}
